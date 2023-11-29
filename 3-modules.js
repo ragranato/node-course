2db42d2194b4259388d0295bd4643a08990fc425
ragranato <robert.granato@gmail.com>
2023-11-28 19:48:23 -0500

    initial commit

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+// CommonJS, every file is a module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const {john, peter} = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+
+require('./7-mind-grenade')
+
+// sayHi('susan')
+// sayHi(john)
+// sayHi(peter);
